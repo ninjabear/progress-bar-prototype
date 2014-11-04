4f7018e556f0fbc8b765d232e02990049531fc3d
Ed Lewis <edward.richard.lewis@gmail.com>
2014-11-04 22:26:30 +0000

    sample working

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <windows.h>
+#include <commctrl.h>
+
+#define ID_BUTTON 1
+#define ID_TIMER 2
 
 // props
 // http://zetcode.com/gui/winapi/firststeps/
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void CenterWindow(HWND);
+HINSTANCE g_hinst;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                     PWSTR pCmdLine, int nCmdShow)
@@ -19,10 +24,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   
   RegisterClassW(&wc);
   CreateWindowW(wc.lpszClassName, 
-  			    L"Center",
+  			    L"Type",
                 WS_OVERLAPPEDWINDOW | WS_VISIBLE,
                 100, 100,
-			    500, 150,
+			    260, 170,
 			    NULL,
 			    NULL,
 			    hInstance, 
@@ -39,22 +44,51 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, 
     WPARAM wParam, LPARAM lParam)
 {
+ static HWND hwndPrgBar;
+  static int i = 1;
+ 
+  INITCOMMONCONTROLSEX InitCtrlEx;
+
+  InitCtrlEx.dwSize = sizeof(INITCOMMONCONTROLSEX);
+  InitCtrlEx.dwICC  = ICC_PROGRESS_CLASS;
+  InitCommonControlsEx(&InitCtrlEx);
+
   switch(msg)  
   {
       case WM_CREATE:
-      {
-          CenterWindow(hwnd);
-          return 0;
-      }
+		  CenterWindow(hwnd);
+          hwndPrgBar = CreateWindowEx(0, PROGRESS_CLASS, NULL, 
+                WS_CHILD | WS_VISIBLE | PBS_SMOOTH,
+                30, 20, 190, 25, hwnd, NULL, g_hinst, NULL);   
+
+          CreateWindow(TEXT("button"), TEXT("Start"), 
+                WS_CHILD | WS_VISIBLE,
+                85, 90, 80, 25, hwnd, (HMENU) 1, g_hinst, NULL);  
+
+          SendMessage(hwndPrgBar, PBM_SETRANGE, 0, MAKELPARAM(0, 150));
+          SendMessage(hwndPrgBar, PBM_SETSTEP, 1, 0 );
+          break;
+
+
+      case WM_TIMER:
+          SendMessage( hwndPrgBar, PBM_STEPIT, 0, 0 );
+          i++;
+          if ( i == 150 ) 
+              KillTimer(hwnd, ID_TIMER);
+          break;
+              
+      case WM_COMMAND:
+          i = 1;
+          SendMessage( hwndPrgBar, PBM_SETPOS, 0, 0 );
+          SetTimer(hwnd, ID_TIMER, 5, NULL);
+          break;
 
       case WM_DESTROY:
-      {
+          KillTimer(hwnd, ID_TIMER);
           PostQuitMessage(0);
-          return 0;
-      }
+          break; 
   }
-
-  return DefWindowProcW(hwnd, msg, wParam, lParam);
+  return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
 void CenterWindow(HWND hwnd)
