5bead43e2e9ecb6744b76dbed349d3596b35c6f7
Ed Lewis <edward.richard.lewis@gmail.com>
2014-11-04 20:29:35 +0000

    first

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	MessageBox(NULL, "Hello world", "Message", MB_OK);
+
+	return 0;
+}
