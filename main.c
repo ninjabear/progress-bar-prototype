1d179a063efb89815898f5a1a895ab74311922e8
Ed Lewis <edward.richard.lewis@gmail.com>
2014-11-04 20:40:20 +0000

    added window creation boilerplate

--- main.c
+++ main.c
@@ -1,9 +1,70 @@
 #include <windows.h>
 
+// props
+// http://zetcode.com/gui/winapi/firststeps/
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void CenterWindow(HWND);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    PWSTR pCmdLine, int nCmdShow)
 {
-	MessageBox(NULL, "Hello world", "Message", MB_OK);
+  MSG  msg;    
+  WNDCLASSW wc = {0};
+  wc.lpszClassName = L"Center";
+  wc.hInstance     = hInstance;
+  wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+  wc.lpfnWndProc   = WndProc;
+  wc.hCursor       = LoadCursor(0, IDC_ARROW);
+  
+  RegisterClassW(&wc);
+  CreateWindowW(wc.lpszClassName, 
+  			    L"Center",
+                WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                100, 100,
+			    500, 150,
+			    NULL,
+			    NULL,
+			    hInstance, 
+			    NULL);  
 
-	return 0;
+  while( GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+
+  return (int) msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, 
+    WPARAM wParam, LPARAM lParam)
+{
+  switch(msg)  
+  {
+      case WM_CREATE:
+      {
+          CenterWindow(hwnd);
+          return 0;
+      }
+
+      case WM_DESTROY:
+      {
+          PostQuitMessage(0);
+          return 0;
+      }
+  }
+
+  return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+void CenterWindow(HWND hwnd)
+{
+    RECT rc;
+    
+    GetWindowRect(hwnd, &rc) ;
+    
+    SetWindowPos(hwnd, 0, 
+        (GetSystemMetrics(SM_CXSCREEN) - rc.right)/2,
+        (GetSystemMetrics(SM_CYSCREEN) - rc.bottom)/2,
+         0, 0, SWP_NOZORDER|SWP_NOSIZE);
 }
